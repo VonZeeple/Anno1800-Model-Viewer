e5c633437831467505ff2436c89e95d800d0375d
VonZeeple <vonzeeple@gmail.com>
2020-04-11 02:03:24 -0400

    Models now rotate properly Signed-off-by: VonZeeple <vonzeeple@gmail.com>

--- renderer.py
+++ renderer.py
@@ -6,6 +6,7 @@ from asset import Asset, TransformerOrientation
 import numpy as np
 import math
 
+
 class Renderer:
     def __init__(self, manager):
         self.asset_manager = manager
@@ -95,11 +96,13 @@ class Renderer:
                 self.render_asset(a)
         # Render the props:
         for pc in asset.prop_containers:
+            glPushMatrix()
             for t in pc.transformers:
                 transformers = Renderer.get_chained_transformers(asset, t)
                 Renderer.apply_transformers(transformers)
             for p in pc.props:
                 self.render_prop(p)
+            glPopMatrix()
         # Render the decal
         glColor3f(1.0, 1.0, 1.0)
         for decal in asset.decals:
@@ -112,7 +115,7 @@ class Renderer:
             for t in decal.transformers:
                 transformers = Renderer.get_chained_transformers(asset, t)
                 Renderer.apply_transformers(transformers)
-            decal_mesh = Mesh.gen_square(extent[0]*2, extent[2]*2)
+            decal_mesh = Mesh.gen_square(extent[0] * 2, extent[2] * 2)
             decal_materials = {'decal': list(decal.materials)[0]}
             self.render_model(decal_mesh, materials=decal_materials)
             glPopMatrix()
@@ -132,6 +135,7 @@ class Renderer:
                     transformer_list += Renderer.get_chained_transformers(asset, t3)
 
         return transformer_list
+
     @staticmethod
     def apply_transformers(transformer_list):
         for t in transformer_list:
@@ -141,8 +145,18 @@ class Renderer:
             rot = t.rotation
             scale = t.scale
             glTranslatef(*pos)
-            glRotatef(*rot)
-            glScalef(*([scale]*3))
+            Renderer.apply_rotation(rot)
+            glScalef(*([scale] * 3))
+
+    @staticmethod
+    def apply_rotation(vec4):
+        #Maybe check if the vector is normalized
+        angle = (math.acos(vec4[3]) * 2)
+        anorm = math.sqrt(vec4[0]**2 + vec4[1]**2 + vec4[2]**2)
+        if anorm != 0 :
+            glRotatef(math.degrees(angle), vec4[0]/anorm, vec4[1]/anorm, vec4[2]/anorm)
+        else:
+            glRotatef(math.degrees(angle), vec4[0], vec4[1], vec4[2])
 
     def render_prop(self, prop):
         if prop.filename not in self.asset_manager.props.keys():
@@ -157,11 +171,10 @@ class Renderer:
         scale = prop.scale
         glPushMatrix()
         glTranslatef(*pos)
-        glRotatef(*rot)
+        Renderer.apply_rotation(rot)
         glScalef(*scale)
         self.render_model(mesh)
         glPopMatrix()
-
 
     def render_model(self, mesh, materials=None):
         if mesh is None:
@@ -197,9 +210,9 @@ class Renderer:
             # TODO:check the size of a vertex
             offset = int(group['offset'])
             size = int(group['size'])
-            glVertexPointer(3, GL_FLOAT, 32, vbo_data + offset*32)
-            glNormalPointer(GL_FLOAT, 32, vbo_data + 12 + offset*32)
-            glTexCoordPointer(2, GL_FLOAT, 32, vbo_data+24 + offset*32)
+            glVertexPointer(3, GL_FLOAT, 32, vbo_data + offset * 32)
+            glNormalPointer(GL_FLOAT, 32, vbo_data + 12 + offset * 32)
+            glTexCoordPointer(2, GL_FLOAT, 32, vbo_data + 24 + offset * 32)
             glDrawArrays(GL_TRIANGLES, 0, int(size))
 
     def render(self, model, asset):
@@ -210,12 +223,11 @@ class Renderer:
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glClearColor(0.5, 0.7, 1, 1)
         glColor3f(1.0, 1.0, 1.0)
-
+        glMatrixMode(GL_MODELVIEW)
         if isinstance(model, Mesh):
             glDisable(GL_TEXTURE_2D)
             self.render_model(model)
         elif isinstance(asset, Asset):
             glEnable(GL_TEXTURE_2D)
             self.render_asset(asset)
-
 
